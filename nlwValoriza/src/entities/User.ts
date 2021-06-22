469edb1c694272ad13a6493276eb224ce9edf56f
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-06-21 23:10:12 -0300

    Aula 02 - NLW

---
+++ nlwValoriza/src/entities/User.ts
@@ -0,0 +1,28 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+ 
+@Entity("users")
+class User {
+  
+  @PrimaryColumn()
+  readonly id: string;
+  
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+
+};
+export { User };
+
+//Entitdade <-> ORM <-> BD (users)
--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,23 +1,8 @@
-import express, { response } from "express";
-import { request } from "http";
+import "reflect-metadata"; 
+import express from "express";
 
-// @types/express
+import "./database";
+
 const app = express();
-/*
- GET => Buscar uma informação 
- POST => Inserir (Criar) uma informação 
- PUT => Alterar uma informação
- DELETE => Remover uma dado
- PATCH => Alterar uma informação especifica
-*/
-app.get("/test", (request, response) => {
-  //Request => Entrando
-  //Response => Saindo
-  return response.send("Olá NLW")
-});
 
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST")
-})
-// http://localhost:3000
-app.listen(3000, () => console.log("Server is running NLW"));
+app.listen(3000, () => console.log("Server is running"));
