f1a76991908bdd05d292bb817d3d148700f1cfa9
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-08-20 20:33:02 -0300

    NLW - atualização

---
+++ node.js
@@ -0,0 +1,64 @@
+************************************** 20/06/21
+
+yarn init -y
+
+yarn add typescript -D
+
+yarn tsc --init
+
+yarn tsc
+
+yarn add express
+
+yarn add @types/express -D
+
+yarn add  ts-node-dev -D
+
+yarn dev
+
+*************************************** 21/06/21
+
+yarn typeorm --help
+
+yarn typeorm migration:run
+
+yarn typeorm migration:revert
+
+yarn typeorm entity:create -n User
+
+yarn add uuid
+
+yarn add @types/uuid -D
+
+*************************************** 22/06/21
+
+yarn add express-async-errors
+
+yarn typeorm migration:create -n CreateTags
+
+yarn typeorm migration:run
+
+*************************************** 24/06/21
+
+yarn add jsonwebtoken
+
+yarn add @types/jsonwebtoken -D
+
+yarn typeorm migration:create -n AlterUserAddPassword
+
+yarn typeorm migration:run
+
+yarn add bcryptjs
+
+yarn add @types/bcryptjs -D
+
+yarn typeorm migration:create -n CreateCompliments
+
+**************************************** 26/06/21
+
+yarn add class-transformer
+
+yarn add cors
+
+
+
